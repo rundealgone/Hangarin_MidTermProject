14fc5823cee7c5d5fa34abd5a73c4d84aa116913
unknown <rjlopezvelete@gmail.com>
2026-04-12 23:55:49 +0800

    add adapter to bypass conflict

---
+++ tasks/templates/tasks/adapter.py
@@ -0,0 +1,16 @@
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.account.utils import perform_login
+from django.contrib.auth.models import User
+
+class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def pre_social_login(self, request, sociallogin):
+        if sociallogin.is_existing:
+            return
+        if 'email' not in sociallogin.account.extra_data:
+            return
+        try:
+            email = sociallogin.account.extra_data['email']
+            user = User.objects.get(email=email)
+            sociallogin.connect(request, user)
+        except User.DoesNotExist:
+            pass
